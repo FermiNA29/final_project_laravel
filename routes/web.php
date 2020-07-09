01c9e3de4addf89308b271b3adbf5d2c5bd0ada8
Muhammad Safii <45284719+muhammaddsafii@users.noreply.github.com>
2020-07-09 14:36:45 +0700

    ubah index dan tambah auth

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
+
+Route::get('/questions', 'QuestionsController@index');
+
+Route::group(['middleware'=>'auth'], function(){
+    //ini buat login
+});
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
